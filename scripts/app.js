8364684ce0f3fc894ffe8ae80062306ca8d447d4
smmakowski <makowski.sm@gmail.com>
2017-08-14 09:39:43 -0700

    add tweet button; start function that autotruncates quote

--- scripts/app.js
+++ scripts/app.js
@@ -1,20 +1,48 @@
 $(document).ready(function() {
-  var chars = {
-    bronn: '',
-    brynden: '',
-    cersei: '',
-    hound: '',
-    jaime: '',
-    littlefinger: '',
-    olenna: '',
-    renly: '',
-    tyrion: '',
+  // json char to
+  // var chars = {
+  //   Bronn: {
+  //     name: 'Ser Bronn of the Blackwater',
+  //     photo: 'https://en.wikipedia.org/wiki/Bronn_(character)#/media/File:Bronn-Jerome_Flynn.jpg',
+  //   },
+  //   'Brynden Tully': {
+  //     name: 'Brynden Tully',
+  //     photo: ''
+  //   },
+  //   Cersei: {
+  //
+  //   },
+  //   'The Hound': {
+  //
+  //   },
+  //   'Jaime Lannister': '',
+  //   Littlefinger: '',
+  //   'Olenna Tyrell': '',
+  //   'Renly Baratheon': '',
+  //   Tyrion: '',
+  //   Varys: '',
+  // }
+  function genTweetURL(quote, char) {
+    var tweet = quote + char;
+    var url = 'https://twitter.com/intent/tweet?hashtags=quotes,GoT&related=freecodecamp&text='
+    if (tweet.length > 128) {
+      var truncated = tweet.slice() // make sure
+    }
+    return url;
   }
-  // GoT Quote API URL => https://github.com/wsizoo/game-of-thrones-quotes
+
   $('#get-quote').on('click', function() {
+    // GoT Quote API URL => https://github.com/wsizoo/game-of-thrones-quotes
     $.getJSON('https://got-quotes.herokuapp.com/quotes', function(json) {
-      $('#show-quote').text('"' + json.quote + '"');
-      $('#show-character').text('- ' + json.character);
+      var quote = '"' + json.quote + '"';
+      var char = '- ' + json.character;
+      $('#show-quote').text(quote);
+      $('#show-character').text(char);
+
+    // change url for <a> tag to reflect new quote
+    // tweet button docs URL => https://dev.twitter.com/web/tweet-button
+
+    $('a').attr('href', 'http://www.bk.com');
     });
   });
 });
