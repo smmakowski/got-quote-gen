88fff57b051c1c79346b3489c1d4933cf7df4634
smmakowski <makowski.sm@gmail.com>
2017-08-14 12:51:29 -0700

    add basic tweet function; achieve mvp

--- scripts/app.js
+++ scripts/app.js
@@ -23,26 +23,27 @@ $(document).ready(function() {
   //   Varys: '',
   // }
   function genTweetURL(quote, char) {
-    var tweet = quote + char;
+    var tweet = quote + ' ' + char;
     var url = 'https://twitter.com/intent/tweet?hashtags=quotes,GoT&related=freecodecamp&text='
     if (tweet.length > 128) {
-      var truncated = tweet.slice() // make sure
+      var diff = (128 - tweet.length - 4);
     }
+    url += tweet;
     return url;
   }
 
   $('#get-quote').on('click', function() {
     // GoT Quote API URL => https://github.com/wsizoo/game-of-thrones-quotes
     $.getJSON('https://got-quotes.herokuapp.com/quotes', function(json) {
-      var quote = '"' + json.quote + '"';
+      var quote = '"<small>' + json.quote + '</small>"';
       var char = '- ' + json.character;
-      $('#show-quote').text(quote);
+      $('#show-quote').html(quote);
       $('#show-character').text(char);
 
     // change url for <a> tag to reflect new quote
     // tweet button docs URL => https://dev.twitter.com/web/tweet-button
-
-    $('a').attr('href', 'http://www.bk.com');
+    var url = genTweetURL(quote, char);
+    $('a').attr('href', url);
     });
   });
 });
