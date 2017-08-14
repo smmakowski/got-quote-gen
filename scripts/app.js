648cbb82900ca16b2743e57b4f151decaba0fe24
smmakowski <makowski.sm@gmail.com>
2017-08-14 00:15:08 -0700

    display quote and basic character name on click

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,20 @@
 $(document).ready(function() {
+  var chars = {
+    bronn: '',
+    brynden: '',
+    cersei: '',
+    hound: '',
+    jaime: '',
+    littlefinger: '',
+    olenna: '',
+    renly: '',
+    tyrion: '',
+  }
   // GoT Quote API URL => https://github.com/wsizoo/game-of-thrones-quotes
   $('#get-quote').on('click', function() {
     $.getJSON('https://got-quotes.herokuapp.com/quotes', function(json) {
-
+      $('#show-quote').text('"' + json.quote + '"');
+      $('#show-character').text('- ' + json.character);
     });
   });
 });
