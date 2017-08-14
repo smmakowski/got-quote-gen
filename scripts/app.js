045c7e1b2354bbefeb11f495b2ef17d7b26c6855
smmakowski <makowski.sm@gmail.com>
2017-08-14 13:20:48 -0700

    refactor click handler to remove unneccesary complexity

--- scripts/app.js
+++ scripts/app.js
@@ -1,49 +1,14 @@
 $(document).ready(function() {
-  // json char to
-  // var chars = {
-  //   Bronn: {
-  //     name: 'Ser Bronn of the Blackwater',
-  //     photo: 'https://en.wikipedia.org/wiki/Bronn_(character)#/media/File:Bronn-Jerome_Flynn.jpg',
-  //   },
-  //   'Brynden Tully': {
-  //     name: 'Brynden Tully',
-  //     photo: ''
-  //   },
-  //   Cersei: {
-  //
-  //   },
-  //   'The Hound': {
-  //
-  //   },
-  //   'Jaime Lannister': '',
-  //   Littlefinger: '',
-  //   'Olenna Tyrell': '',
-  //   'Renly Baratheon': '',
-  //   Tyrion: '',
-  //   Varys: '',
-  // }
-  function genTweetURL(quote, char) {
-    var tweet = quote + ' ' + char;
-    var url = 'https://twitter.com/intent/tweet?hashtags=quotes,GoT&related=freecodecamp&text='
-    if (tweet.length > 128) {
-      var diff = (128 - tweet.length - 4);
-    }
-    url += tweet;
-    return url;
-  }
-
   $('#get-quote').on('click', function() {
-    // GoT Quote API URL => https://github.com/wsizoo/game-of-thrones-quotes
-    $.getJSON('https://got-quotes.herokuapp.com/quotes', function(json) {
-      var quote = '"<small>' + json.quote + '</small>"';
-      var char = '- ' + json.character;
-      $('#show-quote').html(quote);
-      $('#show-character').text(char);
+    var url = 'https://twitter.com/intent/tweet?hashtags=quotes,GoT&related=freecodecamp&text=';
 
-    // change url for <a> tag to reflect new quote
-    // tweet button docs URL => https://dev.twitter.com/web/tweet-button
-    var url = genTweetURL(quote, char);
-    $('a').attr('href', url);
+    $.getJSON('https://got-quotes.herokuapp.com/quotes', function(json) {
+      $('small').text(json.quote);
+      $('#show-character').text('- ' + json.character);
+
+      url += '"' + json.quote + '"' + ' - ' + json.character;
+
+      $('a').attr('href', url);
     });
   });
 });
