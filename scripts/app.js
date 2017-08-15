ca08ba12a351fcdf1979bce762811e85f494256d
smmakowski <makowski.sm@gmail.com>
2017-08-14 18:18:31 -0700

    finalize styling

--- scripts/app.js
+++ scripts/app.js
@@ -1,25 +1,24 @@
 $(document).ready(function() {
   function getQuote() {
+    $('#get-quote').prop('disabled', true);
     $('#show-quote').fadeOut(function() {
       $('#show-character').fadeOut(function() {
         var url = 'https://twitter.com/intent/tweet?hashtags=quotes,GoT&related=freecodecamp&text=';
-              $.getJSON('https://got-quotes.herokuapp.com/quotes', function(json) {
-                $('#show-quote').text(json.quote);
-                $('#show-character').text(json.character);
+        $.getJSON('https://got-quotes.herokuapp.com/quotes', function(json) {
+          $('#show-quote').text(json.quote);
+          $('#show-character').text(json.character);
 
-                url += '"' + json.quote + '"' + ' - ' + json.character;
-                $('a').attr('href', url);
-                $('#show-quote').fadeIn(function() {
-                  $('#show-character').fadeIn(function() {
-
-                  });
-                });
-              });
+          url += '"' + json.quote + '"' + ' - ' + json.character;
+          $('a').attr('href', url);
+          $('#show-quote').fadeIn(function() {
+            $('#show-character').fadeIn(function() {
+              $('#get-quote').prop('disabled', false);
+            });
+          });
+        });
       });
     });
-
   }
-
 
   $('#get-quote').on('click', function() {
     getQuote();
